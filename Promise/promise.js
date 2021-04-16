482387359c325a41887cd7e9a822e50d5663999f
caizihua <1668281469@qq.com>
2021-04-16 19:39:35 +0800

    4/15

--- Promise/promise.js
+++ Promise/promise.js
@@ -1,26 +1,22 @@
-function Promise1(executor) {
+function Promise(executor) {
   this.PromiseState = "pending";
   this.PromiseResult = null;
-  this.callback = [];
-  _this = this;
+  this.callbacks = [];
+  const _this = this;
   function resolve(data) {
     if (_this.PromiseState !== "pending") return;
     _this.PromiseState = "fulfilled";
     _this.PromiseResult = data;
-    setTimeout(() => {
-      _this.callback.forEach((item) => {
-        item.onResolved(data);
-      });
+    _this.callback.forEach((item) => {
+      item.onResolved(data);
     });
   }
   function reject(data) {
     if (_this.PromiseState !== "pending") return;
     _this.PromiseState = "rejected";
     _this.PromiseResult = data;
-    setTimeout(() => {
-      _this.callback.forEach((item) => {
-        item.onRejected(data);
-      });
+    _this.callbacks.forEach((item) => {
+      item.onRejected(data);
     });
   }
   try {
@@ -30,77 +26,108 @@ function Promise1(executor) {
   }
 }
 
-Promise1.prototype.then = function (onResolved, onRejected) {
-  if (this.PromiseState === "fulfilled") {
-    onResolved(this.PromiseResult);
+Promise.prototype.then = function (onResolved, onRejected) {
+  const _this = this;
+  if (typeof onRejected !== "function") {
+    onRejected = (reason) => {
+      throw reason;
+    };
   }
-  if (this.PromiseState === "rejected") {
-    onRejected(this.PromiseResult);
+  if (typeof onResolved !== "function") {
+    onResolved = (value) => value;
   }
-  if (this.PromiseState === "pending") {
-    this.callback.push({ onResolved, onRejected });
-  }
+  return new Promise((resolve, reject) => {
+    function callback(type) {
+      try {
+        let result = type(_this.PromiseResult);
+        if (result instanceof Promise) {
+          result.then(
+            (v) => {
+              resolve(v);
+            },
+            (r) => {
+              reject(r);
+            }
+          );
+        } else {
+          resolve(result);
+        }
+      } catch (err) {
+        reject(err);
+      }
+    }
+    if (this.PromiseState === "fulfilled") {
+      callback(onResolved);
+    }
+    if (this.PromiseState === "rejected") {
+      callback(onRejected);
+    }
+    if (this.PromiseState === "pending") {
+      this.callbacks.push({
+        onResolved: function () {
+          callback(onResolved);
+        },
+        onRejected: function () {
+          callback(onRejected);
+        },
+      });
+    }
+  });
+};
+Promise.prototype.catch = function (onRejected) {
+  return this.then(null, onRejected);
 };
 
-let p1 = new Promise1((resolve, reject) => {
-  resolve("resolve");
-  resolve("FINE");
-});
-let p2 = new Promise1((resolve, reject) => {
-  reject("reject");
-});
-let p3 = new Promise1((resolve, reject) => {
-  throw "error";
-});
-let p4 = new Promise1((resolve, reject) => {
-  setTimeout(() => {
-    resolve("resolve async");
-  }, 0);
-});
+Promise.resolve = function (value) {
+  return new Promise((resolve, reject) => {
+    if (value instanceof Promise) {
+      value.then(
+        (v) => {
+          resolve(v);
+        },
+        (r) => reject(r)
+      );
+    } else {
+      resolve(value);
+    }
+  });
+};
 
-p1.then((value) => {
-  console.log(value);
+Promise.reject = function (reason) {
+  return new Promise((resolve, reject) => {
+    reject(reason);
+  });
+};
+
+Promise.all = function (promises) {
+  return new Promise((resolve, reject) => {
+    let count = 0;
+    let arr = [];
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(
+        (v) => {
+          count++;
+          arr[i] = v;
+          if (count === promises.length) {
+            resolve(arr);
+          }
+        },
+        (r) => {
+          reject(r);
+        }
+      );
+    }
+  });
+};
+
+let q = new Promise((res, rej) => {
+  res("OK");
 });
-p2.then(
-  (value) => {
-    console.log(value);
-  },
-  (reason) => {
-    console.log(reason);
-  }
-);
-p3.then(
-  (value) => {
-    console.log(value);
-  },
-  (reason) => {
-    console.log(reason);
-  }
-);
-p4.then(
-  (value) => {
-    console.log(value);
-  },
-  (reason) => {
-    console.log(reason);
-  }
-);
-/* p5.then(
-  (value) => {
-    console.log(value);
-  },
-  (reason) => {
-    console.log(reason);
-  }
-);
-p5.then(
-  (value) => {
-    console.log(value);
-  },
-  (reason) => {
-    console.log(reason);
-  }
-);
-[1, 2, 3, 4].forEach((item) => {
-  console.log(item);
-}); */
+q.then()
+  .then((v) => {
+    console.log(v);
+    throw "error";
+  })
+  .catch((r) => {
+    console.log(r);
+  });
