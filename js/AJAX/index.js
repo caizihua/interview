44ec6782162d4513747006b83c3e09ac94587d6a
caizihua <1668281469@qq.com>
2021-04-26 22:54:29 +0800

    22.54

---
+++ js/AJAX/index.js
@@ -0,0 +1,31 @@
+const { json } = require("express");
+const express = require("express");
+const app = express();
+
+app.get("/server", (req, res) => {
+  //设置允许跨域
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.send("Hello Express GET");
+});
+
+app.all("/server", (req, res) => {
+  //设置允许跨域
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  //接受所有响应头参数
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.send("Hello Express POST");
+});
+
+app.all("/jsonServer", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  const data = {
+    name: "app",
+  };
+  const str = JSON.stringify(data);
+  res.send(str);
+});
+
+app.listen(3000, () => {
+  console.log("server is running at http://localhost:3000");
+});
