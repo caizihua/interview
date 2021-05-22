07071c0c217fddcb37f85b650cd3f5cb3e5b3fdf
caizihua <1668281469@qq.com>
2021-05-22 23:45:08 +0800

    2021/5/22

--- js/javascript/闭包.js
+++ js/javascript/闭包.js
@@ -13,15 +13,26 @@ function bar() {
 bar();
 bar();
 bar();
-console.log("\n");
+//闭包的一大特性就是能更新外部变量的值，实际上闭包存储的是引用而不是副本
 //毫无疑问当执行一次后a被改变为1，再执行时，内层访问的外层变量已经改变，所以变成2，这就形成了闭包
 
-for (let i = 1; i <= 5; i++) {
+/* for (let i = 1; i <= 5; i++) {
   {
     setTimeout(function test() {
       console.log(i); //>> 1 2 3 4 5
     }, i * 0);
   }
+} */
+//let让每次的i得以保存形成，如果使用var，那么全局变量中的i已经变成了5，所以将全部输出5
+
+function out(val1) {
+  return function (val2) {
+    return val1 + val2;
+  };
 }
 
-//let让每次的i得以保存形成，如果使用var，那么全局变量中的i已经变成了5，所以将全部输出5
+var res = out("I`m ");
+console.log(res("Rose"));
+console.log(res("Jake"));
+//js中的函数包含了比调用它们执行所需要的代码还要多的信息，还存储了可能会引用的定义在内部作用域中的变量。
+//这个函数就是闭包
