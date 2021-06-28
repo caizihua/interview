0cb2c238e6cf646b49f9c3aa87b75c69841479a8
caizihua <1668281469@qq.com>
2021-06-28 22:36:32 +0800

    2021/6/28

--- js/javascript/this中new与显式.js
+++ js/javascript/this中new与显式.js
@@ -3,12 +3,13 @@ function foo(something) {
 }
 
 var obj1 = {}
-
+/* 函数给对象添加了a属性 */
 var bar = foo.bind(obj1)
-
 bar(2)
 console.log(obj1.a)
+/* bar被硬绑定至obj1中 */
 
+/* 实例对象与构造函数 */
 var baz = new bar(3)
 console.log(obj1.a);
 console.log(baz.a);
